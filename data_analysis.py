2ed02915add63d9829baf95ca2ec11e560259f37
Sameer Bhatti <sbhatti915@gmail.com>
2023-06-10 23:58:22 -0400

    Plotted frequencies of levels

--- data_analysis.py
+++ data_analysis.py
@@ -13,13 +13,9 @@ path_to_csv = '/home/sameer/biods220/assign1/diabetic_retinopathy_referral/diabe
 
 df = pd.read_csv(path_to_csv)
 
-# plt.hist(df['level'], bins=5, rwidth=0.6)
-
-# fig, ax = plt.subplots()
 x = df['level'].value_counts()
 levels = list(x.index.values)
 counts = x.tolist()
-# plxt(ax=ax, kind='bar')
 
 plt.bar(levels, counts)
 plt.title('Counts of levels')
